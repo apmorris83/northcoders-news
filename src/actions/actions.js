7c651eca2a424a77d2de2c74bb801618295572c8
Sarah Jane Morris <apmorris83@gmail.com>
2017-03-29 19:25:29 +0100

    progress made with reducer testing

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,18 +1,41 @@
-import * as types from './types';
+import * as types from '../types/types';
 import axios from 'axios';
-import thunk from 'redux-thunk';
+import { ROOT } from '../../config/config';
 
-export function fetchArticlesRequest (data) {
-    type: types.FETCH_ARTICLES_REQUEST,
-    data
+export function getArticles (topic) {
+    return function (dispatch) {
+        const URL = topic
+            ? ROOT + `/topics/${topic}/articles`
+            : ROOT + '/articles';
+        dispatch(fetchArticlesRequest());
+        axios
+            .get(URL)
+            .then(res => {
+                dispatch(fetchArticlesSuccess(res.data.articles));
+            })
+            .catch(err => {
+                dispatch(fetchArticlesError(err));
+            });
+    };
 }
 
-export function fetchArticlesSuccess (data) {
-    type: types.FETCH_ARTICLES_SUCCESS,
-    data
+export function fetchArticlesRequest() {
+    return {
+        type: types.FETCH_ARTICLES_REQUEST
+    };
 }
 
-export function fetchArticlesError (error) {
-    type: types.FETCH_ARTICLES_ERROR,
-    error
+export function fetchArticlesSuccess(data) {
+    return {
+        type: types.FETCH_ARTICLES_SUCCESS,
+        data
+    };
+
+}
+
+export function fetchArticlesError(error) {
+    return {
+        type: types.FETCH_ARTICLES_ERROR,
+        error
+    };
 }
--- src/reducers/articles.reducer.js
+++ src/reducers/articles.reducer.js
@@ -6,81 +6,29 @@ const initialState = {
     error: null
 }
 
-// function articlesReducer(prevState = initialState, action) {
-//     switch (action.type) {
-        
-//         case types.FETCH_ARTICLES_REQUEST: {
-//             const newState = Object.assign({}, prevState, {
-//                 loading: true
-//             });
-//             return newState;
-//         }
-//         case types.FETCH_ARTICLES_SUCCESS: {
-//             const newState = Object.assign({}, prevState, {
-//                 loading: false,
-//                 byId: normaliseData(action.data)
-//             });
-//             return newState;
-//         }
-//         case types.FETCH_ARTICLES_ERROR: {
-//             const newState = Object.assign({}, prevState, {
-//                 loading: false,
-//                 error: action.data
-//             });
-//             return newState;
-//         }
-//         default: {
-//             return prevState;
-//         }
-//     }
-// }
-
-
-
-
 export function articlesReducer(prevState = initialState, action) {
     const newState = Object.assign({}, prevState);
-
     if (!action) return newState;
-    
+
     if (action.type === types.FETCH_ARTICLES_REQUEST) {
-        // newState.loading = true;
-        const newState = Object.assign({}, prevState, {
-                loading: true
-            });
-        
+        newState.loading = true;
     }
 
     if (action.type === types.FETCH_ARTICLES_SUCCESS) {
         return Object.assign({}, prevState, {
-            byId: normaliseData(action.data),
-            loading: false
+            loading: false,
+            byId: normaliseData(action.data)
         });
-        
     }
-
+    if (action.type === types.FETCH_ARTICLES_ERROR) {
+        error: action.error
+    }
     return newState;
 }
-    // if (action.type === types.FETCH_ARTICLES_ERROR) {
-    //     return Object.assign({}, prevState, {
-    //         error: action.error
-    //     });
-    // }
-//     const newState = Object.assign({}, prevState);
 
-//   switch (action.type) {
-//     case types.FETCH_ARTICLES_REQUEST:
-//       newState.loading = true;
-//       return newState;
-
-//     default:
-//       return newState;
-//   }
-// }
-
-function normaliseData(data) {
-    return data.reduce((acc, item) => {
-        acc[item._id] = item;
+function normaliseData(arr) {
+    return arr.reduce(function (acc, elem) {
+        acc[elem._id] = elem;
         return acc;
     }, {});
 }
